3046df262f37afad204b40c8ad3cc43029b765c6
Panat Taranat <panat.taranat@gmail.com>
2021-05-04 02:47:19 -0400

    Feat(frontend): Add Login page placeholder

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,13 @@
 import Vue from "vue";
 import Router from "vue-router";
 import HomePage from "./../components/HomePage.vue";
+import Login from "./../components/Login.vue";
 
 Vue.use(Router);
 
 export default new Router({
-  routes: [{ path: "/", component: HomePage }],
+  routes: [
+    { path: "/", component: HomePage },
+    { path: "/login", component: Login },
+  ],
 });
