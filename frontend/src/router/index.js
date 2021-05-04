96c747459a98c5d3458b314712bc87041442f5de
Panat Taranat <panat.taranat@gmail.com>
2021-05-04 02:43:04 -0400

    Feat(frontend): Import Vue-Router and use it in App.vue

---
+++ frontend/src/router/index.js
@@ -0,0 +1,9 @@
+import Vue from "vue";
+import Router from "vue-router";
+import HomePage from "./../components/HomePage.vue";
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [{ path: "/", component: HomePage }],
+});
