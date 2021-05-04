33bfd4c0f2baa8527342437d019594c1eab408be
Panat Taranat <panat.taranat@gmail.com>
2021-05-04 02:36:50 -0400

    feat: Add bootstrap CSS, navbar, image component
    - Imports Bootstrap CSS library
    - Add NavBar component
    - Add Image component
    - Use v-for to load images from URLs

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
+    <Navbar />
     <HomePage />
   </div>
 </template>
 
 <script>
+import Navbar from "./components/Navbar.vue";
 import HomePage from "./components/HomePage.vue";
 
 export default {
   name: "app",
   components: {
     HomePage,
+    Navbar,
   },
 };
 </script>
---
+++ frontend/src/components/ImageCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card">
+    <img class="card-img-top" :src="img" alt="" />
+    <div class="card-body">
+      <h5 class="card-title">{{ img }}</h5>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ImageCard",
+  props: ["img"],
+};
+</script>
+
+<style>
+</style>
