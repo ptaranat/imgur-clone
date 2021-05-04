545f89736d856ed54d0ae64539aa2b21eb81d84d
Panat Taranat <panat.taranat@gmail.com>
2021-05-04 02:50:14 -0400

    feat: Add dynamic routing for single images

---
+++ frontend/src/components/Single.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>Single Image: {{ $route.params.id }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Single",
+};
+</script>
+
+<style scoped>
+</style>
