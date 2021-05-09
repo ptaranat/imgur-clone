bd53c20136915c0afc62bdb550953b7b8797a8ee
Panat Taranat <panat.taranat@gmail.com>
2021-05-09 14:15:35 -0400

    feat: Display all images in s3 bucket

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -3,7 +3,14 @@
     <div class="container pt-4">
       <div class="row">
         <div v-for="image in latest" v-bind:key="image" class="col-lg-4">
-          <ImageCard :img="image" />
+          <!-- <ImageCard :img="image" /> -->
+          <ImageCard
+            v-bind:img="
+              'https://dev-image-repo-bucket.s3.us-east-2.amazonaws.com/' +
+              image
+            "
+            alt=""
+          />
         </div>
       </div>
     </div>
@@ -11,6 +18,7 @@
 </template>
 
 <script>
+import axios from "axios";
 import ImageCard from "./ImageCard.vue";
 
 export default {
@@ -20,14 +28,19 @@ export default {
   },
   data() {
     return {
-      latest: [
-        "https://picsum.photos/400/200",
-        "https://picsum.photos/400/200",
-        "https://picsum.photos/400/200",
-        "https://picsum.photos/400/200",
-        "https://picsum.photos/400/200",
-      ],
+      latest: [],
     };
+  },
+  created() {
+    axios
+      .get("https://75v2tuitm6.execute-api.us-east-2.amazonaws.com/dev/list")
+      .then((response) => {
+        console.log(response);
+        this.latest = response.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   },
 };
 </script>
