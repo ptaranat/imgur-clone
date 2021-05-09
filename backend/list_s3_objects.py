6809128653f88914f1d86877e5cc35124cebb4b5
Panat Taranat <panat.taranat@gmail.com>
2021-05-09 13:39:53 -0400

    feat: Change list lambda to return list of keys in bucket

--- backend/list_s3_objects.py
+++ backend/list_s3_objects.py
@@ -1,18 +1,32 @@
 import boto3
+import decimal
 import json
 
 bucket = "dev-image-repo-bucket"
 s3 = boto3.client("s3")
 
 
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, decimal.Decimal):
+            return int(obj)
+        return super(DecimalEncoder, self).default(obj)
+
+
 def list(event, context):
     try:
+        keys = []
         body = s3.list_objects_v2(Bucket=bucket)
+        for obj in body["Contents"]:
+            keys.append(obj["Key"])
 
         response = {
             "statusCode": 200,
-            "headers": {"Access-Control-Allow-Origin": "*"},
-            "body": json.dumps(body, default=str),
+            "headers": {
+                "Access-Control-Allow-Origin": "*",
+                "Content-Type": "application/json",
+            },
+            "body": json.dumps(keys, cls=DecimalEncoder, default=str),
         }
     except Exception as e:
         response = {
