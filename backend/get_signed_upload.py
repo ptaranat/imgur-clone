dd5196dbbb08da96164900855cfa5a47fc7cd00d
Panat Taranat <panat.taranat@gmail.com>
2021-05-09 01:09:19 -0400

    fix: Fix signed upload with correct params
    The parameter for generate_presigned_url cannot have "Expires". Keeping "ContentType" is ok.

--- backend/get_signed_upload.py
+++ backend/get_signed_upload.py
@@ -6,20 +6,23 @@ s3 = boto3.client("s3")
 
 
 def request_upload_URL(event, context):
-    params = json.loads(event["body"])
-    parts = s3.generate_presigned_post(
-        Bucket=os.environ["BUCKET"],
-        Key=params["name"],
-        Fields={
-            "Content-Type": params["type"],
-            "Expires": 3600,
-            "acl": "public-read",
-        },
+    body = json.loads(event["body"])
+    s3_params = {
+        "Bucket": os.environ["BUCKET"],
+        "Key": body["name"],
+        "ContentType": body["type"],
+        "ACL": "public-read",
+    }
+
+    url = s3.generate_presigned_url(
+        ClientMethod="put_object",
+        Params=s3_params,
     )
-    url = parts["url"]
     response = {
         "statusCode": 200,
-        "headers": {"Access-Control-Allow-Origin": "*"},
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+        },
         "body": json.dumps({"uploadURL": url}),
     }
     return response
