c69a2a93738522e7c645e30a28eb309a1aefede7
Panat Taranat <panat.taranat@gmail.com>
2021-05-09 17:59:50 -0400

    test: Add tests for list_obj and request_upload_URL

---
+++ backend/tests/test_get_signed_upload.py
@@ -0,0 +1,3 @@
+import pytest
+
+from backend.get_signed_upload import request_upload_URL
---
+++ backend/tests/test_list_s3_objects.py
@@ -0,0 +1,11 @@
+import pytest
+import json
+from backend.list_s3_objects import list_obj
+
+
+def test_list_obj():
+    event = ""
+    context = ""
+    result = list_obj(event, context)
+    assert result["statusCode"] == 200
+    assert isinstance(json.loads(result["body"]), list)
